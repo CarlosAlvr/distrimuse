c74d2314a6cc13e193c228d84d7a2488f45c8bc4
CarlosAlvr <calvarez@gti.uvigo.es>
2025-02-26 12:17:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Activa el entorno virtual
-. /zenohvenv/bin/activate
+/zenohvenv/bin/activate
 # Ejecuta el script; usamos exec para reemplazar el proceso shell
 exec python /distrimuse/acelerometro.py
