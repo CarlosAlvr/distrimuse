4df2e1885f5ef7090ff332ec39eb0a0a1a8c7c15
Anxobrv <abravo@gti.uvigo.es>
2025-03-26 16:41:27 +0100

    Update reconocimiento.py

--- reconocimiento.py
+++ reconocimiento.py
@@ -61,11 +61,11 @@ def main(conf: zenoh.Config, key: str):
     #os.system("Opening session...")
     with zenoh.open(conf) as session:
         env_input = os.environ.get('DISTRIMUSE_INPUT_0')
-            if env_input is None:
-                os.system("Error: La variable de entorno 'Distrimuse_input_0' no está definida.")
+        if env_input is None:
+            os.system("Error: La variable de entorno 'Distrimuse_input_0' no está definida.")
         env_output = os.environ.get('DISTRIMUSE_OUTPUT_0')
-            if env_output is None:
-                os.system("Error: La variable de entorno 'Distrimuse_output_0' no está definida.")
+        if env_output is None:
+            os.system("Error: La variable de entorno 'Distrimuse_output_0' no está definida.")
         #print(f"Declaring Subscriber on '{key}'...")
         #print("Declaring Publisher on 'casa/habitacion1/deteccion'...")
         #pub = session.declare_publisher("casa/habitacion1/deteccion")
