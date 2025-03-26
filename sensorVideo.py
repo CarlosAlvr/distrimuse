167995c8764c3b0dff2255631d783df9b47f776b
Anxobrv <abravo@gti.uvigo.es>
2025-03-26 16:47:46 +0100

    Update sensorVideo.py

--- sensorVideo.py
+++ sensorVideo.py
@@ -33,11 +33,11 @@ def main(conf: zenoh.Config):
 
     with zenoh.open(conf) as session:
         env_input = os.environ.get('DISTRIMUSE_INPUT_0')
-            if env_input is None:
-                os.system("Error: La variable de entorno 'Distrimuse_input_0' no está definida.")
+        if env_input is None:
+            os.system("Error: La variable de entorno 'Distrimuse_input_0' no está definida.")
         env_output = os.environ.get('DISTRIMUSE_OUTPUT_0')
-            if env_output is None:
-                os.system("Error: La variable de entorno 'Distrimuse_output_0' no está definida.")
+        if env_output is None:
+            os.system("Error: La variable de entorno 'Distrimuse_output_0' no está definida.")
         # print("Declaring Subscriber on 'casa/persona1/caida'...")
 
         # print("Declaring Publisher on 'casa/habitacion1/video'...")
