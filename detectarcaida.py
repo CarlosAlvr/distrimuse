e82b67006c52919f0feb31a16ab288c09f99164c
GonzaloFdezLiberato <gfernandez@gti.uvigo.es>
2025-03-31 09:47:29 +0200

    Update detectarcaida.py
    ingles

--- detectarcaida.py
+++ detectarcaida.py
@@ -6,9 +6,7 @@ import os
 import json
 
 def detectar_caida(ax, ay, az):
-    """
-    Detecta una caída basándose en los valores de aceleración.
-    """
+
     umbral_min = 3.0  # Umbral mínimo de aceleración brusca indicando caída libre
     umbral_max = 12.0 # Umbral máximo que puede indicar un impacto
     aceleracion_total = (ax**2 + ay**2 + az**2) ** 0.5
@@ -17,26 +15,26 @@ def detectar_caida(ax, ay, az):
 def main(conf: zenoh.Config):
     env_input = os.environ.get('DISTRIMUSE_INPUT_0')
     if env_input is None:
-        print("Error: La variable de entorno 'DISTRIMUSE_INPUT_0' no está definida.")
+        print("Error: The environment variable 'DISTRIMUSE_INPUT_0' is not defined.")
         return
 
     env_output = os.environ.get('DISTRIMUSE_OUTPUT_0')
     if env_output is None:
-        print("Error: La variable de entorno 'DISTRIMUSE_OUTPUT_0' no está definida.")
+        print("Error: The environment variable 'DISTRIMUSE_OUTPUT_0' is not defined.")
         return
 
-    print(f"Entradas/salidas definidas: {env_input} -> {env_output}")
+    print(f"Defined inputs/outputs: {env_input} -> {env_output}")
 
     # Inicializar el log de Zenoh
     zenoh.init_log_from_env_or("error")
 
     try:
         with zenoh.open(conf) as session:
-            print("Sesión Zenoh iniciada.")
+            print("Zenoh session started.")
 
             # Declarar publisher y subscriber usando las variables de entorno
             pub = session.declare_publisher(env_output)
-            print("Publicador declarado.")
+            print("Declared publisher.")
 
             def listener(sample: zenoh.Sample):
                 try:
@@ -47,23 +45,23 @@ def main(conf: zenoh.Config):
                     caida = detectar_caida(ax, ay, az)
                     pub.put(str(caida))
                     if caida == 1:
-                        print("¡Caída detectada!")
+                        print("¡Fall detected!")
                 except Exception as e:
-                    print(f"Error procesando mensaje: {e}")
+                    print(f"Error processing message: {e}")
 
             session.declare_subscriber(env_input, listener)
-            print("Subscriptor declarado.")
+            print("Declared subscriber.")
 
             while True:
                 time.sleep(1)
 
     except KeyboardInterrupt:
-        print("Saliendo...")
+        print("Exited...")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="detectar_caida",
-        description="Escucha datos del acelerómetro y detecta caídas."
+        description="Listen to accelerometer data and detect falls."
     )
     common.add_config_arguments(parser)
     args = parser.parse_args()
