0d1952a8d50a7616c1dcbc59a1acbcbc49edb11b
GonzaloFdezLiberato <gfernandez@gti.uvigo.es>
2025-04-09 11:48:56 +0200

    Update accelerometer.py

--- accelerometer.py
+++ accelerometer.py
@@ -19,21 +19,22 @@ def main(conf):
         env_output = os.environ.get('DISTRIMUSE_OUTPUT_0')
         if env_output is None:
             os.system("Error: The environment variable 'Distrimuse_output_0' is not defined.")
-        os.system(f"The input variable is: {env_output}")
+        os.system(f"Input: {env_output}")
         pub = session.declare_publisher(env_output)
-        os.system("Publishing accelerometer data every second...")
-        
+        os.system("echo Publishing accelerometer data every second...")
+
         try:
             while True:
                 ax, ay, az = acceleration()
-                data = json.dumps({'X axis': round(ax, 2), 'Y axis': round(ay, 2), 'Z axis': round(az, 2)})
+                data = json.dumps({'ax': round(ax, 2), 'ay': round(ay, 2), 'az': round(az, 2)})
                 pub.put(data)
-                os.system(f"Published: {data} in {env_output}")
+                os.system(f"echo Published: {data}")
                 time.sleep(1)
         except KeyboardInterrupt:
-            os.system("The app is closing...")
+            os.system("echo Exited...")
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(prog="simulador_acelerometro", description="Publishes simulated accelerometer data.")
     zenoh_config= os.environ.get('DISTRIMUSE_CONFIG')
     os.system(zenoh_config)
